68d86cb89ef7e38984a44a896269e12951d51e38
Sébastien Putier <sputier@gmail.com>
2020-04-11 01:07:45 +0200

    Initial commit

---
+++ WpfApp/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using WebApp;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IHost _host;
+
+        public App()
+            => _host = Host.CreateDefaultBuilder()
+                           .ConfigureServices(services => services.AddHostedService<AspNetAppHostedService>())
+                           .Build();
+
+        private async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            await _host.StartAsync();
+        }
+
+        private async void Application_Exit(object sender, ExitEventArgs e)
+        {
+            using (_host)
+                await _host.StopAsync();
+        }
+    }
+
+    internal class AspNetAppHostedService : IHostedService
+    {
+        private IHost _aspNetAppHost;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _aspNetAppHost = Host.CreateDefaultBuilder(null)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                              .UseUrls("https://localhost:12345");
+                }).Build();
+
+            await _aspNetAppHost.RunAsync();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            using (_aspNetAppHost)
+            {
+                // The host application may hang when calling await _aspNetAppHost.StopAsync(); 
+                // To prevent it, we need to force the ASP.NET app to die killing its IHostLifetime service :-)
+                // See https://github.com/dotnet/extensions/issues/1363 for more informations
+
+                var lifetime = _aspNetAppHost.Services.GetService<IHostLifetime>() as IDisposable;
+                lifetime?.Dispose();
+
+                return Task.CompletedTask;
+            }
+        }
+    }
+}
---
+++ WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            var response = await new HttpClient(clientHandler).GetAsync("https://localhost:12345/weatherforecast");
+            string content = await response.Content.ReadAsStringAsync();
+
+            weatherTextBlock.Text = JToken.Parse(content).ToString(Formatting.Indented); ;
+        }
+    }
+}
